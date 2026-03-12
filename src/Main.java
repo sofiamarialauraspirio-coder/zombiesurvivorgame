5533a40907211a04c09f11458786022ea2185ce3
Vironee <salvatore.virone03@community.unipa.it>
2026-03-12 17:38:02 +0100

    NP-10 Test di collegamento tra VS Code e Jira

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Il gioco Zombie vs Survivor è pronto per essere programmato!");
+        // Questo è un test di collegamento con Jira
     }
 }
