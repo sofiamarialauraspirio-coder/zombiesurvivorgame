5652d92ca81fa0d3160251796442aa59fda23953
Vironee <salvatore.virone03@community.unipa.it>
2026-03-12 17:45:44 +0100

    NP-10 Primo test di collegamento

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Il gioco Zombie vs Survivor è pronto per essere programmato!");
         // Questo è un test di collegamento con Jira
+        // test di collegamento
     }
 }
